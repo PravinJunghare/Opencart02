dece200dcd4d784fec972c1f6ed3ec75e37d2ac7
Pravin Junghare <pravinjunghare01@gmail.com>
2026-02-10 14:12:13 +0530

    Day3-Added _AppConstants_ElementUtil_AccountsPage_AccPageTest_PropertiesFile_PageChaining.

--- src/main/java/com/qa/opencart/pages/SearchPage.java
+++ src/main/java/com/qa/opencart/pages/SearchPage.java
@@ -1,5 +1,12 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class SearchPage {
+	private WebDriver driver;
+
+	public SearchPage(WebDriver driver) {
+		this.driver=driver;
+	}
 
 }
